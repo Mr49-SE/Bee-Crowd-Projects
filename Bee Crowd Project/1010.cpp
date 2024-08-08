a2989ac1ddc71bff3e404bd8d9e0c780c0b7742f
Mr49-SE <suicidesquad895@gmail.com>
2024-08-09 04:51:27 +0600

    First

--- Bee Crowd Project/1010.cpp
+++ Bee Crowd Project/1010.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
-
-
-
+int code1,num1,code2,num2;
+double cost1,cost2,total;
+cin >> code1 >> num1 >> cost1 ;
+cin>> code2 >> num2>> cost2;
+total=cost1*num1 + cost2*num2;
+cout<<fixed <<setprecision(2) << "VALOR A PAGAR: R$ "<<total<<endl;
     return 0;
     
 }
